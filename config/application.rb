5010fd953e40447f10c30f6125416dc42e188c01
yasu1224 <yasu1224x@gmail.com>
2020-06-01 17:08:57 +0900

    update

---
+++ config/application.rb
@@ -0,0 +1,17 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module PokeyaruApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+    config.app_generators.scaffold_controller = :scaffold_controller
+  end
+end
