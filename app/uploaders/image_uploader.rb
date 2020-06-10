828a58e2d6f53d6da8529a09c12f803a601ce123
yasu1224 <yasu1224x@gmail.com>
2020-06-10 18:14:24 +0900

    トップページ作成

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [500, 500]
 
   version :thumb do
-    process resize_to_fill: [100,100]
+    process resize_to_fill: [115,164]
   end
 
   storage :file
