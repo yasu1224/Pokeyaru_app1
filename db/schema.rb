71ecef88cce19b4deaadba288fefbb7a3430e0e9
yasu1224 <yasu1224x@gmail.com>
2020-06-23 20:42:48 +0900

    update,schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_081358) do
+ActiveRecord::Schema.define(version: 2020_06_23_113152) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 2020_06_01_081358) do
     t.string "name"
     t.string "place"
     t.date "release"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
