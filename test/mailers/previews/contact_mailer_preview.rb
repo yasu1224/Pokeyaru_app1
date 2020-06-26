4d7a3aae44fa47b1724fa7c2236c3bc99bcf917d
yasu1224 <yasu1224x@gmail.com>
2020-06-26 15:08:36 +0900

    Gmailの送信設定

--- test/mailers/previews/contact_mailer_preview.rb
+++ test/mailers/previews/contact_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
 class ContactMailerPreview < ActionMailer::Preview
-
+  def contact
+    contact = Contact.new(name: "テストさん", email: "test@test", message: "お問い合わせ内容")
+    ContactMailer.send_mail(contact)
+  end
 end
